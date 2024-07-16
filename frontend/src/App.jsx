1cc4bd33791116706ebec3e6b12fc139042ca4f5
kshivashankarai <k.shivashankar.ai@gmail.com>
2024-07-16 10:03:17 +0530

    Page navigation with react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import Login from './components/Login'
+import Home from './components/Home'
+import Register from './components/Register'
+import Navbar from './components/Navbar'
+import About from './components/About'
+
+import { Routes, Route, useLocation } from 'react-router-dom'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const location = useLocation()
+  const pathname = location.pathname
+  const nonavbar = pathname === '/register' || pathname === '/'
+
+  const withoutnavbar =
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+
+  const withnavbar =
+    <Navbar content={
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    } />
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        nonavbar ? withoutnavbar : withnavbar
+      }
     </>
   )
 }
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div>
+            This is a Register Page
+        </div>
+    )
+}
+
+export default Register
